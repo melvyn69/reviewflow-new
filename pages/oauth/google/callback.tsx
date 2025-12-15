d25484737ad1b579614a0fb4d8f84ce5b0a67372
melvyn69 <melvynbenichou@gmail.com>
2025-12-15 19:30:28 +0100

    Fix callback: read code from search OR hash + debug mount

--- pages/oauth/google/callback.tsx
+++ pages/oauth/google/callback.tsx
@@ -2,9 +2,27 @@ import React, { useEffect } from 'react';
 import { supabase } from '../../../lib/supabase';
 
 const OAuthCallback = () => {
+  console.log('CALLBACK_PAGE_MOUNTED', window.location.href);
+
   useEffect(() => {
     (async () => {
-      const code = new URLSearchParams(window.location.search).get('code');
+      const href = window.location.href;
+
+      // 1) normal: ?code=...
+      let code = new URL(href).searchParams.get('code');
+
+      // 2) hashrouter: #/oauth/google/callback?code=...
+      if (!code) {
+        const hash = window.location.hash || '';
+        const qs = hash.includes('?') ? hash.split('?')[1] : '';
+        code = new URLSearchParams(qs).get('code');
+      }
+
+      if (!code) {
+        alert('NO_CODE_FOUND');
+        return;
+      }
+
       const redirectUri = `${window.location.origin}/oauth/google/callback`;
 
       const { error } = await supabase.functions.invoke('social_oauth', {
@@ -19,7 +37,7 @@ const OAuthCallback = () => {
     })();
   }, []);
 
-  return <div />;
+  return <div>Callback Google en cours…</div>;
 };
 
 export default OAuthCallback;
